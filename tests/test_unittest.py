edad0e5cefd0c4b5c7898cb26817cfff0bd4f8e9
hana.boukricha <boukricha.hana@gmail.com>
2021-03-10 12:12:40 +0100

    version 0.0.4 released! Successfully returns data for a given year and age.

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -8,11 +8,12 @@ from childcaredatabw import child_care_data_csv # The code to test
 class Test_Read_Filter_Data(unittest.TestCase):
     
     def test_data_frames(self):
-        # test should succeed by giving the following file_name and file_format as input
+        # test should succeed by giving the following example file_name, file_format, year, location, and age as input
         file_name = "/home/hana/develop/projects/my_api_projects/Child_Care_Data/tests/child_care_data.csv"
         file_format = "csv"
-        year = "2007" # possible values from 2007 to 2020. Note that possible test optimization: iterate through all year values!
-        location = "Insgesamt" # possible values Tageseinrichtung, Tagespflege, Insgesamt. Note that possible test optimization: iterate through all location values!
+        year = "2007" # possible values from '2007' to '2020'. Note that possible test optimization: iterate through all year values!
+        location = "Insgesamt" # possible values 'Tageseinrichtung', 'Tagespflege', 'Insgesamt'. Note that possible test optimization: iterate through all location values!
+        age = "unter 3" # possible values 'unter 3', '3 bis unter 6', '6 bis unter 14'. Note that possible test optimization: iterate through all age values!
 
         # test should fail by giving the following file_name as input
         # corresponding exception is catched by implemented code
@@ -35,6 +36,11 @@ class Test_Read_Filter_Data(unittest.TestCase):
         #location = "any" # location not included within data
         #location = ""
 
+        # test should fail by giving the following age as input
+        # corresponding exception is catched by implemented code
+        #age = "any" # age not included within data
+        #age = ""
+
         # tesing read data frames
         data = child_care_data.read_data(file_name, file_format)
         data_csv = child_care_data_csv.read_data_csv(file_name) 
@@ -49,6 +55,11 @@ class Test_Read_Filter_Data(unittest.TestCase):
         data_year_location = child_care_data.get_data_year_location(year, location, data, file_format)
         data_year_location_csv = child_care_data_csv.get_data_year_location_csv(year, location, data)
         assert_frame_equal(data_year_location, data_year_location_csv)
+
+        # testing get_data_year_age data frames
+        data_year_age = child_care_data.get_data_year_age(year, age, data, file_format)
+        data_year_age_csv = child_care_data_csv.get_data_year_age_csv(year, age, data)
+        assert_frame_equal(data_year_age, data_year_age_csv)
 
 if __name__ == '__main__':
     unittest.main()
