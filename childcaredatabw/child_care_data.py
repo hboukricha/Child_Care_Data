924642209a54eabb8face4b4e47e9c6895fbf740
hana.boukricha <boukricha.hana@gmail.com>
2021-03-25 22:58:17 +0100

    code refactoring done

--- childcaredatabw/child_care_data.py
+++ childcaredatabw/child_care_data.py
@@ -20,67 +20,78 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 # @Author: hana.boukricha
 # @Date:   2021-03-06 22:12:41
 # @Last Modified by:   hana.boukricha
-# @Last Modified time: 2021-03-25 20:21:06
+# @Last Modified time: 2021-03-25 22:53:34
 
-""" This module provides an integrable api library to retrieve data on child care numbers in Baden Wuertemberg """
+""" This module provides an integrable api library to retrieve data on child 
+care numbers in Baden Wuertemberg """
 
 import re as regex
 import pandas as pd
 
+
 def read_data(file_name, file_format):
-    """ read the file data from str file_name with str file_format and return a data structure. Currently supported file_format is csv. """
-    
+    """ read the file data from str file_name with str file_format and return a data structure. 
+    Currently supported file_format is csv. """
+
     if file_format == "csv":
         data = __read_data_csv(file_name)
     else:
         # if file_format not supported set data object to None (null)
         data = None
-        raise Exception("function child_care_data.read_data: Specified file_format", file_format ,"is not supported, please look at the api user manual for supported formats!")
-    
+        raise Exception("function child_care_data.read_data: Specified file_format", file_format, 
+        "is not supported, please look at the api user manual for supported formats!")
+
     return data
 
+
 def get_data_year(year, data_structure):
-    """ read data_structure and return data_year including data for str year. Read data_structure with respect to str data_format. """
+    """ read data_structure and return data_year including data for str year. Read data_structure 
+    with respect to str data_format. """
+    
     try:
-        data_structure[year]
+        data_year = data_structure[year]
     except Exception as error:
         print(error)
     else:
-        print(year)
-        print(data_structure[year])
+        print(data_year)
+        return data_year
+
 
 def get_data_year_location(year, location, data_structure):
-    """ read data_structure and return data_year_location including data for str year and str location. Read data_structure with respect to str data_format. """
+    """ read data_structure and return data_year_location including data for str year and str location. 
+    Read data_structure with respect to str data_format. """
+    
     try:
-        data_structure[year][location]
+        data_year_location = data_structure[year][location]
     except Exception as error:
         print(error)
     else:
-        print(year)
-        print(location)
-        print(data_structure[year][location])
+        print(data_year_location)
+        return data_year_location
+
 
 def get_data_year_location_age(year, location, age, data_structure):
-    """ read data_structure and return data_year_age including data for str year and str age. Read data_structure with respect to str data_format. """
+    """ read data_structure and return data_year_age including data for str year and str age. Read data_structure 
+    with respect to str data_format. """
+    
     try:
-        data_structure[year][location][age]
+        data_year_location_age = data_structure[year][location][age]
     except Exception as error:
         print(error)
     else:
-        print(year)
-        print(location)
-        print(age)
-        print(data_structure[year][location][age])
+        print(data_year_location_age)
+        return data_year_location_age
+
 
 def __read_data_csv(file_name):
     """ read csv formatted data from str file_name and returns a pandas dataframe csv_data. """
-    try: 
-        csv_data = pd.read_csv(file_name, delimiter=";", skiprows=2, header=None) 
-    
+    try:
+        csv_data = pd.read_csv(file_name, delimiter=";", skiprows=2, header=None)
+
     except Exception as error:
         print(error)
     else:
@@ -91,12 +102,12 @@ def __read_data_csv(file_name):
         num_columns = len(csv_data.columns)
         data_dict = {}
         # going through all rows and columns of the dataframe
-        for i in range(num_rows) :
-            for j in range (num_columns) :
+        for i in range(num_rows):
+            for j in range(num_columns):
                 element = csv_data.iloc[i, j]
                 # regular expression matching any year within dataframe
                 year_exp = "20[0,1,2]"
-                if not pd.isna(element) :
+                if not pd.isna(element):
                     result = regex.match(year_exp, element)
                     if result:
                         data_dict[csv_data.iloc[i, j]] = {}
@@ -122,3 +133,4 @@ def __read_data_csv(file_name):
                         data_dict[csv_data.iloc[i, j]]['Insgesamt'][csv_data.iloc[i+5, j].strip()] = csv_data.iloc[i+5, j+3]
                         data_dict[csv_data.iloc[i, j]]['Insgesamt'][csv_data.iloc[i+6, j].strip()] = csv_data.iloc[i+6, j+3]
         return data_dict
+
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,9 +1,25 @@
-from childcaredatabw import child_care_data # The code to test
+# @Author: hana.boukricha
+# @Date:   2021-03-25 13:06:27
+# @Last Modified by:   hana.boukricha
+# @Last Modified time: 2021-03-25 22:51:50
 
-def test_child_care_data (data_regression): 
-    
+from childcaredatabw import child_care_data  # The code to test
+
+
+def test_child_care_data(data_regression):
+
     # read the data from csv file
     data_current = child_care_data.read_data("./tests/child_care_data.csv", "csv")
-    
+
     # check regression of provided data
     data_regression.check(data_current)
+
+    # check input/output of api functions
+    assert child_care_data.get_data_year("2007", data_current) == {'Tageseinrichtung': {'Betreute Kinder insgesamt': '379.734', 'unter 3': '26.978', '3 bis unter 6': '281.627', '6 bis unter 14': '71.129', 'Betreute Kinder mit Mittagsverpflegung': '109.388'}, 'Tagespflege': {
+        'Betreute Kinder insgesamt': '13.287', 'unter 3': '6.049', '3 bis unter 6': '3.031', '6 bis unter 14': '4.207', 'Betreute Kinder mit Mittagsverpflegung': ' X'}, 'Insgesamt': {'Betreute Kinder insgesamt': '393.021', 'unter 3': '33.027', '3 bis unter 6': '284.658', '6 bis unter 14': '75.336', 'Betreute Kinder mit Mittagsverpflegung': ' X'}}
+    
+    assert child_care_data.get_data_year_location("2007", "Tageseinrichtung", data_current) == {'Betreute Kinder insgesamt': '379.734', 'unter 3': '26.978', '3 bis unter 6': '281.627',
+            '6 bis unter 14': '71.129', 'Betreute Kinder mit Mittagsverpflegung': '109.388'}
+
+    assert child_care_data.get_data_year_location_age("2007", "Tageseinrichtung", "unter 3", data_current) == "26.978"
+
